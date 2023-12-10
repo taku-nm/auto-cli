5f8949ffa37d1766044e9f15e89df23622ca4507
taku <lukasbb4015@gmail.com>
2023-12-10 12:01:40 +0100

    ci: bump version

--- auto-cli.bat
+++ auto-cli.bat
@@ -87,7 +87,7 @@ if exist "%localappdata%\revanced-cli\input.json" (
 )
 
 REM script version check
-set batVersion=3.3
+set batVersion=3.4
 for /f %%i in ('powershell -NoProfile -NonInteractive -Command "(Get-Content -Raw '%inputJson%' | ConvertFrom-Json).batVersion"') do ( set "jsonBatVersion=%%i" )
 if /i '%batVersion%' == '%jsonBatVersion%' (
 	echo  [92m Script up-to-date!   Version %batVersion% [0m
